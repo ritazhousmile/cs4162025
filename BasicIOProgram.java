c6f89c8f5c2ac3ee9f158d74d1e9aac6ad45a0d5
ritazhousmile <ritazhousmile@gmail.com>
2025-01-30 18:13:45 -0500

    basic io program change

--- BasicIOProgram.java
+++ BasicIOProgram.java
@@ -3,29 +3,31 @@
  * @version 01/30/2025
  */
 import java.util.Scanner;
+
+//basic io program
 public class BasicIOProgram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter two whole numbers separated by one or more spaces:");
+        System.out.println("Enter two whole numbers\nseparated by one or more spaces:");
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
 
-        System.out.println("You entered " + num1 + " and " +num2);
+        System.out.println("You entered " + num1 +  " and " + num2);
 
         scanner.nextLine();
 
-        System.out.println("Next Enter two numbers. A decimal point is OK.");
+        System.out.println("Next enter two numbers.\nA decimal point is OK.");
         double num3 = scanner.nextDouble();
         double num4 = scanner.nextDouble();
-        System.out.println("You entered " + num3 + " and " +num4);
+        System.out.println("You entered " + num3 + " and " + num4);
 
         scanner.nextLine();
 
-        System.out.println("Next enter two words: ");
+        System.out.println("Next enter two words:");
         String word1 = scanner.next();
         String word2 = scanner.next();
-        System.out.println("You entered \"" + word1 + " \" and \" " + word2 + "\"");
+        System.out.println("You entered \"" + word1 + "\" and \"" + word2 + "\"");
 
         scanner.nextLine();
 
