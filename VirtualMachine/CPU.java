79d92cbd6c4cff1336ab7102a6c169c0c6760e2b
ritazhousmile <ritazhousmile@gmail.com>
2025-03-12 23:25:48 -0400

    virtualMachinelab

--- VirtualMachine/CPU.java
+++ VirtualMachine/CPU.java
@@ -1,19 +1,147 @@
-
 /**
-* A class to model a CPU in a simple Virtual Machine
-*/
+ * A class to model a CPU in a simple Virtual Machine
+ */
 public class CPU {
-
+    // Renamed to follow Java naming conventions
+    private int ip;
+    private int is;
+    private int r0;
+    private int r1;
+    private Boolean running;
+    
+    /**
+     * Default constructor.
+     * Initializes the CPU object state with all registers set to 0.
+     */
     public CPU() {
-        //TODO: implement
+        ip = 0;
+        is = 0;
+        r0 = 0;
+        r1 = 0;
+        running = true;
     }
 
+    /**
+     * Runs the CPU with the given memory.
+     * 
+     * @param mem The memory to use
+     * @param debugMode Whether to print debug information
+     * @throws IllegalStateException If an unknown instruction is encountered
+     */
     public void run(Memory mem, boolean debugMode) throws IllegalStateException {
-        //TODO: implement
+        int step = 0; 
+        while (running) {
+            // Wrap around the IP instead of stopping execution
+            if (ip < 0 || ip >= Memory.DEFAULT_SIZE) {
+                ip = ip & 0xF; // Wrap around to keep IP in range 0-15
+            }
+            
+            is = mem.read(ip);
+            if (debugMode) {
+                System.out.println("STEP " + step + ":");
+                System.out.println(this);
+                System.out.println(mem);
+                System.out.println("----------");
+            }
+
+            // Execute: Execute the instruction based on the IS value
+            switch (is) {
+                case 0: // Halt
+                    running = false;
+                    break;
+                case 1: // R0 = R0 + R1
+                    r0 = (r0 + r1) & 0xF; // Ensure wrap-around using & 0xF
+                    ip++;
+                    break;
+                case 2: // R0 = R0 - R1
+                    r0 = (r0 - r1) & 0xF;
+                    ip++;
+                    break;
+                case 3: // Increment R0
+                    r0 = (r0 + 1) & 0xF;
+                    ip++;
+                    break;
+                case 4: // Increment R1
+                    r1 = (r1 + 1) & 0xF;
+                    ip++;
+                    break;
+                case 5: // Decrement R0
+                    r0 = (r0 - 1) & 0xF;
+                    ip++;
+                    break;
+                case 6: // Decrement R1
+                    r1 = (r1 - 1) & 0xF;
+                    ip++;
+                    break;
+                case 7: 
+                    ip++;
+                    break;
+                case 8: // Print X
+                    ip++;
+                    int x = mem.read(ip);
+                    System.out.println(x);
+                    ip++;
+                    break;
+                case 9: // R0 = MEMORY[X]
+                    ip++;
+                    x = mem.read(ip);
+                    r0 = mem.read(x) & 0xF;
+                    ip++;
+                    break;
+                case 10: // R1 = MEMORY[X]
+                    ip++;
+                    x = mem.read(ip);
+                    r1 = mem.read(x) & 0xF;
+                    ip++;
+                    break;
+                case 11: // MEMORY[X] = R0
+                    ip++;
+                    x = mem.read(ip);
+                    mem.write(x, r0);
+                    ip++;
+                    break;
+                case 12: // MEMORY[X] = R1
+                    ip++;
+                    x = mem.read(ip);
+                    mem.write(x, r1);
+                    ip++;
+                    break;
+                case 13: // JUMP X
+                    ip++;
+                    x = mem.read(ip);
+                    ip = x;
+                    break;
+                case 14: // JUMP X IF R0 == 0
+                    ip++;
+                    x = mem.read(ip);
+                    if (r0 == 0) {
+                        ip = x;
+                    } else {
+                        ip++;
+                    }
+                    break;
+                case 15: // JUMP X IF R0 != 0
+                    ip++;
+                    x = mem.read(ip);
+                    if (r0 != 0) {
+                        ip = x;
+                    } else {
+                        ip++;
+                    }
+                    break;
+                default:
+                    throw new IllegalStateException("Unknown instruction: " + is);
+            }
+            step++;
+        }
     }
 
+    /**
+     * Returns a string representation of the CPU state.
+     * 
+     * @return A string with IP, IS, R0, and R1 values
+     */
     public String toString() {
-        //TODOL implement
-        return "";
+        return String.format("IP=%2d, IS=%2d, R0=%2d, R1=%2d", ip, is, r0, r1);
     }
 }
--- VirtualMachine/Memory.java
+++ VirtualMachine/Memory.java
@@ -2,34 +2,102 @@ import java.util.Scanner;
 
 /**
  * A class to model a Simple Virtual machine Memory
- *
  */
 public class Memory {
+    /**
+     * Default size of the VM memory.
+     */
+    public static final int DEFAULT_SIZE = 16;
+    
+    private int[] memory;
 
+    /**
+     * Default constructor.
+     * Creates an array of integers to represent the VM memory and initializes each memory cell to 0.
+     */
     public Memory() {
-        //TODO: implement
+        memory = new int[DEFAULT_SIZE];
     }
 
+    /**
+     * One argument constructor.
+     * Creates an array of integers to represent the VM memory and initializes each
+     * memory cell to a value from the Scanner.
+     * 
+     * @param s Scanner containing the input values
+     * @throws IllegalStateException if there are too many values in the input
+     */
     public Memory(Scanner s) {
-        //TODO: implement
+        memory = new int[DEFAULT_SIZE]; 
+        int count = 0;
+    
+        while (s.hasNextInt()) { 
+            if (count >= DEFAULT_SIZE) {
+                throw new IllegalStateException("Memory overflow: Too many values in input");
+            }
+            memory[count] = s.nextInt();
+            count++;
+        }
     }
-
+    
+    /**
+     * Returns the value stored at the specified address.
+     * 
+     * @param address the memory address to read from
+     * @return the value at the specified address
+     * @throws IllegalStateException if the address is outside the valid range
+     */
     public int read(int address) throws IllegalStateException {
-        //TODO: Implement
-        return -1;
+        if (address < 0 || address >= memory.length) {
+            throw new IllegalStateException("Memory access error: Invalid address " + address);
+        }
+        return memory[address];
     }
 
+    /**
+     * Writes a value to the specified address.
+     * 
+     * @param address the memory address to write to
+     * @param value the value to write
+     * @throws IllegalStateException if the address is outside the valid range
+     */
     public void write(int address, int value) throws IllegalStateException {
-        //TODO: Implement
+        if (address < 0 || address >= memory.length) {
+            throw new IllegalStateException("Memory access error: Invalid address " + address);
+        }
+        memory[address] = value;
     }
 
+    /**
+     * Returns a string representation of the memory.
+     * 
+     * @return a string showing addresses and values in a table format
+     */
     public String toString() {
-        //TODO: Implement
-        return "";
+        StringBuilder sb = new StringBuilder();
+
+        // Address row
+        sb.append(" address: |");
+        for (int i = 0; i < memory.length; i++) {
+            sb.append(String.format(" %2d |", i));  
+        }
+        sb.append("\n");
+
+        // Value row
+        sb.append("   value: |");
+        for (int i = 0; i < memory.length; i++) {
+            sb.append(String.format(" %2d |", memory[i]));
+        }
+
+        return sb.toString(); 
     }
 
+    /**
+     * Returns a copy of the raw memory array.
+     * 
+     * @return a copy of the memory array
+     */
     public int[] rawMemory() {
-        //TODO: implement
-        return null;
+        return memory.clone();  
     }
 }
